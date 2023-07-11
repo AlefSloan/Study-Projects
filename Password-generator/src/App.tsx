28fe616ae038083401d94ea1e0e6bcb6495e33ea
alefsloan <alef_sloan@hotmail.com>
2023-07-11 10:52:44 -0300

    feat: clipboard copy of the password when clicked

--- Password-generator/src/App.tsx
+++ Password-generator/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { nanoid } from 'nanoid'
+import copy from 'clipboard-copy';
+
 
 import './App.css'
 
@@ -10,12 +12,19 @@ export function App() {
     setPassword(nanoid())
   }
 
+  function handleCopyPassword() {
+    copy(password)
+      .then(() => alert("Senha copiada com sucesso!"))
+      .catch(() => alert("Ocorreu um erro na cópia, por favor tente novamente!"))
+
+  }
+
   return (
     <div id="main-container">
       <h1>Gerador de senhas</h1>
       <div id="password-container">
-        <button onClick={ () => handlePassword() }>Gerar senha</button>
-        <span>{password}</span>
+        <button onClick={ () => handlePassword() } >Gerar senha</button>
+        <span onClick={handleCopyPassword} >{password}</span>
       </div>
     </div>
   )
